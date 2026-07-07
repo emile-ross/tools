9e433af15757a75b7899602fb2324f592d82a535
emile-ross <emileross@tutanota.de>
2026-07-07 02:04:15 -0400

    feat: implement Bools and word list struct
    this implements my own version of bools since this is c90 compliant and
    there is no way to use the stdbool.h header file

--- src/get_words.c
+++ src/get_words.c
@@ -1,10 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+
+#define UINT8MAX (255)
+#define UINT16MAX (65535)
+
+/* my own implementation of the stdbool.h library (C99 extension) */
+typedef enum
+{
+	True = 1,
+	False = 0
+} Bool;
+
+typedef struct
+{
+	char *source_file;
+	Bool success;
+	uint32_t total_words;
+	uint32_t valid_words;
+} word_list;
+
+word_list parse_cmd(int num_args, char *arg[]);
 
 int main(int argc, char *argv[])
 {
 	if (argc > 1)
 	{
+		parse_cmd(argc, argv);
 	}
 	else
 	{
