0f113b269bda7472249dde7120251b269a5812e6
emileross <emileross@tutanota.de>
2026-07-10 23:53:39 -0400

    feat: add flag_type enum
    this defines the typedef enum used for all flags

--- src/renaming.c
+++ src/renaming.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 
+
+typedef enum
+{
+	undefined_flag = 0,
+	char_flag,
+	all_files_flag,
+	single_file_flag,
+	invalid_flag,
+} flag_type;
+
+
 /* this will rename files with certain characters in the filename */
 int main(int argc, char *argv[])
 {
