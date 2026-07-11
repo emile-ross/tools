6581d0df9df509509d15c22dd3609f0a58ec22c3
emileross <emileross@tutanota.de>
2026-07-10 23:48:11 -0400

    feat: add argument checking number
    this checks the number of arguments in the command

--- src/renaming.c
+++ src/renaming.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
 
 /* this will rename files with certain characters in the filename */
-int main(void)
+int main(int argc, char *argv[])
 {
-	printf("Provide the filename\n");
-	/* get input */
+	if (argc > 2)
+	{
+	}
+	else
+	{
+		/* this is not intended behaviour for now.. */
+		return 1;
+		printf("Provide the filename\n");
+		/* get input */
+	}
 
 	return 0;
 }
