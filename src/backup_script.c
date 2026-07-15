f5a1a0a1cd1d47d91e2a88a64b1e7857167cb595
emileross <emileross@tutanota.de>
2026-07-14 23:34:47 -0400

    feat: loop through args for flag matching
    matches the arguments to a valid flag and also set some outcomes and
    store the data
    
    d

--- src/backup_script.c
+++ src/backup_script.c
@@ -4,6 +4,7 @@
 #include <string.h>
 
 #define min_args (3)
+#define base_args (1)	/* the number of useless arguments preceeding the command (with info) */
 typedef enum
 {
 	True = 1,
@@ -15,10 +16,26 @@ int main(int argc, char *argv[])
 	Bool full_backup = True;
 	if (argc > min_args)
 	{
+		uint8_t i = 0;
+		for (; i < argc; i++)
+		{
+			if (strcmp(argv[i], "-a") == 0 || strcmp(argv[i], "--all") == 0)
+			{
+				full_backup = False;
+			}
+		}
 	}
 	else
 	{
 		full_backup = False;
+	}
+	if (full_backup)
+	{
+		/* do full backup */
+	}
+	else
+	{
+		/* backup specific configs */
 	}
 	return 0;
 }
