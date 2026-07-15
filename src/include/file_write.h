b05799a798231a7309ffb2db83d1f2c3e7fb7f95
emileross <emileross@tutanota.de>
2026-07-15 01:45:09 -0400

    fix: memory leak in file_write on error
    free buffer when an error is triggered (only if the buffer isn't a null
    pointer)

--- src/include/file_write.h
+++ src/include/file_write.h
@@ -1,5 +1,6 @@
 #include <stdarg.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 size_t ssnprintf(char *buffer, size_t buffer_size, const char *format, ...)
 {
@@ -8,6 +9,15 @@ size_t ssnprintf(char *buffer, size_t buffer_size, const char *format, ...)
 	
 	va_start(ap, format);
 	return_value = (size_t)vsnprintf(buffer, buffer_size, format, ap);
+	if (buffer_size != 0 && return_value > buffer_size)
+	{
+		fprintf(stderr, "Truncated write, writing %zu bytes to a buffer of size %zu", return_value, buffer_size);
+		if (buffer != NULL)
+		{
+			free(buffer);
+		}
+		exit(1);
+	}
 	va_end(ap);
 	return return_value;
 }
