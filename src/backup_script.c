ff612b38e4084f63845a64993acb534819cd6c3b
emileross <emileross@tutanota.de>
2026-07-14 23:33:47 -0400

    feat: add Bool replacement for c89
    this bool replacement is c89 compatible

--- src/backup_script.c
+++ src/backup_script.c
@@ -1,6 +1,24 @@
+#define POSIX_C_SOURCE 199506L
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+
 #define min_args (3)
+typedef enum
+{
+	True = 1,
+	False = 0
+} Bool;
 
 int main(int argc, char *argv[])
 {
+	Bool full_backup = True;
+	if (argc > min_args)
+	{
+	}
+	else
+	{
+		full_backup = False;
+	}
 	return 0;
 }
