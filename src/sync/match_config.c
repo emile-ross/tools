b41c5285a93a3c705f3cd7145549c058c7400e74
emileross <emileross@tutanota.de>
2026-07-17 22:13:56 -0400

    feat: add checks in match_config.c
    this adds the file backup checks for 2 options psw and bookmarks

--- src/sync/match_config.c
+++ src/sync/match_config.c
@@ -7,6 +7,20 @@ int match_config(uint8_t buf_i, void *buf_arr[], Bool backup_data_arr[NUM_DATA_B
 
 	if (backup_data_arr[gitconfig_data])
 	{
+		data_backed_up = True;
+		filename_data->source_filepath = gitconfig_src;
+		filename_data->destination_filepath = gitconfig_dst;
+		backup_file_conversion(buf_arr, &buf_i, filename_data);
+	}
+
+	if (backup_data_arr[bookmarks_data])
+	{
+		data_backed_up = True;
+		filename_data->source_filepath = bookmarks_src;
+		filename_data->destination_filepath = bookmarks_dst;
+		backup_file_conversion(buf_arr, &buf_i, filename_data);
+	}
+
 	}
 
 	return 0;
