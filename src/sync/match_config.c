18924c3a0967d7517eec25f7c52ec0bc9cfb6b77
emileross <emileross@tutanota.de>
2026-07-18 21:42:48 -0400

    fix: invalid function prototype
    the function prototype uses a 16bit wide integer instead of 8

--- src/sync/match_config.c
+++ src/sync/match_config.c
@@ -1,7 +1,7 @@
 #include "header.h"
 #include "head_backup.h"
 
-int match_config(uint8_t buf_i, void *buf_arr[], Bool backup_data_arr[NUM_DATA_BACKUP], struct filename_type *filename_data)
+int match_config(uint16_t buf_i, void *buf_arr[], Bool backup_data_arr[NUM_DATA_BACKUP], struct filename_type *filename_data)
 {
 	Bool data_backed_up = False;
 
