141c9016bb524ef47ed7fb4b8adb6d3845cb44d8
emileross <emileross@tutanota.de>
2026-07-19 13:53:21 -0400

    fix: invalid backup_file_conversion() use
    pass a pointer instead of the whole array

--- src/sync/match_config.c
+++ src/sync/match_config.c
@@ -9,7 +9,7 @@ int match_config(uint16_t buf_i, void *buf_arr[], Bool backup_data_arr[NUM_DATA_
 		data_backed_up = True;
 		filename_data->source_filepath = gitconfig_src;
 		filename_data->destination_filepath = gitconfig_dst;
-		backup_file_conversion(buf_arr, &buf_i, filename_data);
+		backup_file_conversion(&buf_arr, &buf_i, filename_data);
 	}
 
 	if (backup_data_arr[bookmarks_data])
@@ -17,7 +17,7 @@ int match_config(uint16_t buf_i, void *buf_arr[], Bool backup_data_arr[NUM_DATA_
 		data_backed_up = True;
 		filename_data->source_filepath = bookmarks_src;
 		filename_data->destination_filepath = bookmarks_dst;
-		backup_file_conversion(buf_arr, &buf_i, filename_data);
+		backup_file_conversion(&buf_arr, &buf_i, filename_data);
 	}
 
 	if (backup_data_arr[passwords_data])
@@ -25,7 +25,7 @@ int match_config(uint16_t buf_i, void *buf_arr[], Bool backup_data_arr[NUM_DATA_
 		data_backed_up = True;
 		filename_data->source_filepath = passwords_src;
 		filename_data->destination_filepath = passwords_dst;
-		backup_file_conversion(buf_arr, &buf_i, filename_data);
+		backup_file_conversion(&buf_arr, &buf_i, filename_data);
 	}
 	else if (!data_backed_up)
 	{
