164d535e99bc305da7cdca1e3ab93ebc8749d664
emileross <emileross@tutanota.de>
2026-07-27 22:48:05 -0400

    feat: implement data backup in fbackup()
    executes the backup_file() function passing in the correct struct

--- src/sync/config.c
+++ src/sync/config.c
@@ -2,23 +2,6 @@
 
 /* choose the name of your password database (including its path) 
  * make sure you don't include the home path unless you've disabled use_home_dir_src */
-char *mfa_auths_src = "export.2fa";
-char *mfa_auths_dst = "backup/2fa/%s-2fa.2fa";
-
-char *passwords_src = "passwords.kdbx";
-char *passwords_dst = "backup/passwords/%s-passwords.kdbx";
-
-char *bookmarks_src = "bookmarks.json";
-char *bookmarks_dst = "backup/bookmarks/%s-bookmarks.json";
-
-char *gitconfig_src = ".gitconfig";
-char *gitconfig_dst = "backup/git/.%s-gitconfig";
-
-char *dns_logs_src = "logs/dns.log";
-char *dns_logs_dst = "backup/dns-logs/%s-dns.log";	
-
-char *wifi_logs_src = "logs/wifi_log.log";
-char *wifi_logs_dst = "backup/logs/%s-wifi.log";	
 
 const Bool use_home_dir_src = True;
 const Bool use_home_dir_dst = True;
--- src/sync/file_backup.c
+++ src/sync/file_backup.c
@@ -6,13 +6,14 @@ int fbackup(struct filename_type *filename_data, data_types backup_type)
 	{
 		case all_backup_opt:
 		case gitconfig_data:
+			backup_file(&gitconfig_files, filename_data);
 
 			if (!(backup_type == all_backup_opt))
 				break;
 			else
 				__attribute__ ((fallthrough));
 		case passwords_data:
-			backup_file(&passwords_backup, filename_data);
+			backup_file(&passwords_files, filename_data);
 
 			if (!(backup_type == all_backup_opt))
 				break;
@@ -27,18 +28,21 @@ int fbackup(struct filename_type *filename_data, data_types backup_type)
 			else
 				__attribute__ ((fallthrough));
 		case bookmarks_data:
+			backup_file(&bookmarks_files, filename_data);
 
 			if (!(backup_type == all_backup_opt))
 				break;
 			else
 				__attribute__ ((fallthrough));
 		case wifi_logs_data:
+			backup_file(&wifi_logs_files, filename_data);
 
 			if (!(backup_type == all_backup_opt))
 				break;
 			else
 				__attribute__ ((fallthrough));
 		case dns_logs_data:
+			backup_file(&dns_logs_files, filename_data);
 
 			if (!(backup_type == all_backup_opt))
 				break;
