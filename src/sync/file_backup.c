f9e44a0a0c50fab35f809f1eadaef13a25b220a4
emileross <emileross@tutanota.de>
2026-07-27 20:00:51 -0400

    fix: remove duplicate case in switch
    cannot have a duplicate case in a switch

--- src/sync/file_backup.c
+++ src/sync/file_backup.c
@@ -5,14 +5,6 @@ int fbackup(struct filename_type *file_data, data_types backup_type)
 	switch (backup_type)
 	{
 		case all_backup_opt:
-		case mfa_auths_data:
-			backup_file(&mfa_auth_backup, &file_data);
-			backup_file(&aegis_backup, &file_data);
-
-			if (!(backup_type == all_backup_opt))
-				break;
-			else
-				__attribute__ ((fallthrough));
 		case gitconfig_data:
 
 			if (!(backup_type == all_backup_opt))
@@ -26,6 +18,8 @@ int fbackup(struct filename_type *file_data, data_types backup_type)
 			else
 				__attribute__ ((fallthrough));
 		case mfa_auths_data:
+			backup_file(&mfa_auth_backup, file_data);
+			backup_file(&aegis_backup, file_data);
 
 			if (!(backup_type == all_backup_opt))
 				break;
