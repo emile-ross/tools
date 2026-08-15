9de38eab75a17209f74a79ff7fb6539a14fd3023
emileross <emileross@tutanota.de>
2026-08-14 22:50:44 -0400

    fix: invalid backup_file() declaration
    this removes the invalid parameters passed into the backup_file()
    function

--- src/sync/file_backup.c
+++ src/sync/file_backup.c
@@ -6,43 +6,43 @@ int fbackup(data_types backup_type)
 	{
 		case all_backup_opt:
 		case gitconfig_data:
-			backup_file(&gitconfig_files, &file_data);
+			backup_file(&gitconfig_files);
 
 			if (!(backup_type == all_backup_opt))
 				break;
 			else
 				__attribute__ ((fallthrough));
 		case passwords_data:
-			backup_file(&passwords_files, &file_data);
+			backup_file(&passwords_files);
 
 			if (!(backup_type == all_backup_opt))
 				break;
 			else
 				__attribute__ ((fallthrough));
 		case mfa_auths_data:
-			backup_file(&mfa_auth_backup, &file_data);
-			backup_file(&aegis_backup, &file_data);
+			backup_file(&mfa_auth_backup);
+			backup_file(&aegis_backup);
 
 			if (!(backup_type == all_backup_opt))
 				break;
 			else
 				__attribute__ ((fallthrough));
 		case bookmarks_data:
-			backup_file(&bookmarks_files, &file_data);
+			backup_file(&bookmarks_files);
 
 			if (!(backup_type == all_backup_opt))
 				break;
 			else
 				__attribute__ ((fallthrough));
 		case wifi_logs_data:
-			backup_file(&wifi_logs_files, &file_data);
+			backup_file(&wifi_logs_files);
 
 			if (!(backup_type == all_backup_opt))
 				break;
 			else
 				__attribute__ ((fallthrough));
 		case dns_logs_data:
-			backup_file(&dns_logs_files, &file_data);
+			backup_file(&dns_logs_files);
 
 			if (!(backup_type == all_backup_opt))
 				break;
