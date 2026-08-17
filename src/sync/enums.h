055375ef5f0fcab0f9a50d55fe4de853e8c88b9d
emileross <emileross@tutanota.de>
2026-08-17 00:26:38 -0400

    refactor: add enums.h header file
    this adds the enums.h file

---
+++ src/sync/enums.h
@@ -0,0 +1,19 @@
+typedef enum
+{
+	True = 1,
+	False = 0
+} Bool;
+
+typedef enum
+{
+	gitconfig_data = 0,
+	passwords_data,
+	mfa_auths_data,
+	bookmarks_data,
+	wifi_logs_data,
+	nvim_conf_data,
+	sway_conf_data,
+	dns_logs_data,
+	all_backup_opt
+} data_types;
+
