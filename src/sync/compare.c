0066004df151207219bd5609faf065103f6dea31
emileross <emileross@tutanota.de>
2026-08-17 16:38:12 -0400

    feat: compare.c -> creation of scmp()
    creates the scmp() function in compare.c
    used for comparing flags to a string

--- src/sync/compare.c
+++ src/sync/compare.c
@@ -2,15 +2,31 @@
 
 Bool cmp(char *argument, const char *short_flag, const char *long_flag)
 {
+	/* compare argument to the short flag 
+	 * returns true if the argument matches the flag */
 	if (strcmp(argument, short_flag) == 0)
 	{
 		return True;
 	}
 
+	/* compare argument to the long flag 
+	 * returns true if the argument matches the flag */
 	if (strcmp(argument, long_flag) == 0)
 	{
 		return True;
 	}
 
 	return False;
+}
+
+Bool scmp(char *argument, const char *flag)
+{
+	if (strcmp(argument, flag) == 0)
+	{
+		return True;
+	}
+	else
+	{
+		return False;
+	}
 }
