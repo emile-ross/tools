fc298257398a85125134ba8c188c45eea96812ee
emileross <emileross@tutanota.de>
2026-08-17 14:02:37 -0400

    feat: cmd() fn used for comparing flags
    this function is used to simplify comparing arguments to long and short
    flags

--- src/sync/compare.c
+++ src/sync/compare.c
@@ -1,1 +1,16 @@
 #include "header.h"
+
+Bool cmp(char *argument, const char *short_flag, const char *long_flag)
+{
+	if (strcmp(argument, short_flag) == 0)
+	{
+		return True;
+	}
+
+	if (strcmp(argument, long_flag) == 0)
+	{
+		return True;
+	}
+
+	return False;
+}
