a7ca83339b1472bb15288026cf43fb42aecd51e2
emileross <emileross@tutanota.de>
2026-08-17 01:16:27 -0400

    feat: add get_path() function
    this creates the get_path function in path.c

--- src/sync/path.c
+++ src/sync/path.c
@@ -1,2 +1,12 @@
 #include "header.h"
 
+char *get_path(void)
+{
+	size_t max_length = 256;
+
+	char *user_path = malloc(max_length);
+	if (fgets(user_path, (int)max_length, stdin) == NULL) 
+	{
+		fprintf(stderr, "Failed to get the user path\n");
+	}
+}
