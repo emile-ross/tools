21f34c0bbdb53c97f038c1fb223cc0e6f68ef86a
emileross <emileross@tutanota.de>
2026-08-18 03:01:25 -0400

    refactor: file_backup.c move inside condition
    moves everthing inside of a condition in file_backup

--- src/sync/file_backup.c
+++ src/sync/file_backup.c
@@ -11,68 +11,71 @@ int fbackup(struct backup_args args, data_types *backup_type[])
 {
 	switch (backup_type)
 	{
-	case all_backup_opt:
-	case gitconfig_data:
-		backup_file(&gitconfig_files);
+		switch (backup_type[i])
+		{
+		case all_backup_opt:
+		case gitconfig_data:
+			backup_file(&gitconfig_files);
 
-		if (!(backup_type == all_backup_opt))
+			if (!(backup_type[i] == all_backup_opt))
+				break;
+			else
+				__attribute__ ((fallthrough));
+		case passwords_data:
+			backup_file(&passwords_files);
+
+			if (!(backup_type == all_backup_opt))
+				break;
+			else
+				__attribute__ ((fallthrough));
+		case mfa_auths_data:
+			backup_file(&mfa_auth_backup);
+			backup_file(&aegis_backup);
+
+			if (!(backup_type == all_backup_opt))
+				break;
+			else
+				__attribute__ ((fallthrough));
+		case bookmarks_data:
+			backup_file(&bookmarks_files);
+
+			if (!(backup_type == all_backup_opt))
+				break;
+			else
+				__attribute__ ((fallthrough));
+		case wifi_logs_data:
+			backup_file(&wifi_logs_files);
+
+			if (!(backup_type == all_backup_opt))
+				break;
+			else
+				__attribute__ ((fallthrough));
+		case nvim_conf_data:
+			backup_file(&nvim_conf_files);
+
+			if (!(backup_type == all_backup_opt))
+				break;
+			else
+				__attribute__ ((fallthrough));
+
+		case dns_logs_data:
+			backup_file(&dns_logs_files);
+
+			if (!(backup_type == all_backup_opt))
+				break;
+			else
+				__attribute__ ((fallthrough));
+		case sway_conf_data:
+			backup_file(&sway_conf_files);
+
+			if (!(backup_type == all_backup_opt))
+				break;
+			else
+				__attribute__ ((fallthrough));
+
+		default:
 			break;
-		else
-			__attribute__ ((fallthrough));
-	case passwords_data:
-		backup_file(&passwords_files);
-
-		if (!(backup_type == all_backup_opt))
-			break;
-		else
-			__attribute__ ((fallthrough));
-	case mfa_auths_data:
-		backup_file(&mfa_auth_backup);
-		backup_file(&aegis_backup);
-
-		if (!(backup_type == all_backup_opt))
-			break;
-		else
-			__attribute__ ((fallthrough));
-	case bookmarks_data:
-		backup_file(&bookmarks_files);
-
-		if (!(backup_type == all_backup_opt))
-			break;
-		else
-			__attribute__ ((fallthrough));
-	case wifi_logs_data:
-		backup_file(&wifi_logs_files);
-
-		if (!(backup_type == all_backup_opt))
-			break;
-		else
-			__attribute__ ((fallthrough));
-	case nvim_conf_data:
-		backup_file(&nvim_conf_files);
-
-		if (!(backup_type == all_backup_opt))
-			break;
-		else
-			__attribute__ ((fallthrough));
-
-	case dns_logs_data:
-		backup_file(&dns_logs_files);
-
-		if (!(backup_type == all_backup_opt))
-			break;
-		else
-			__attribute__ ((fallthrough));
-	case sway_conf_data:
-		backup_file(&sway_conf_files);
-
-		if (!(backup_type == all_backup_opt))
-			break;
-		else
-			__attribute__ ((fallthrough));
-
-	default:
-		break;
+		}
 	}
 	return 0;
 }
