ff86ff878a3370e52e167ae036a832ef0dced287
emileross <emileross@tutanota.de>
2026-08-22 00:04:44 -0400

    feat: backup.c write directory path to buffer
    writes the directory path to the buffer and then checks it

--- sync/backup.c
+++ sync/backup.c
@@ -33,10 +33,10 @@ void backup_file(struct filebackup_data *filename_data)
 	char *home_path = "%s/%s";
 	char *backup_home_path = "%s/backup/%s";
 
-	char *file_path = bmalloc(NULL, "%s/%s/%s", file_data.home_string, "backup", );
+	char *file_path = bmalloc(NULL, "%s/%s/%s", file_data.home_string, "backup", filename_data->destination_directory);
 	if (!(file_check(file_path)))
 	{
-		system("mkdir -p %s", dir);
+		system("mkdir -p %s", file_path);
 	}
 
 	char *src_file = NULL;
